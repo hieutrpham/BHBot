fc3f2ffad5183cf13f671e3e5c25498ccc735748
hieutrpham <37964079+hieutrpham@users.noreply.github.com>
2019-05-22 19:34:53 -0400

    add a log message

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ raid = Raid(vision, controller)
 def run():
     num_raid = int(input('How many raids to run? '))
     for i in range(num_raid):
+        log(f'Start Raid {i+1}')
         raid.run()
         log(f'Raid {i+1} is done')
 
