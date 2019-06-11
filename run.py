0d06d8de2441f717bc3373a45bbbe9c3f0353947
hieutrpham <37964079+hieutrpham@users.noreply.github.com>
2019-06-11 16:59:48 -0400

    add an argparser

--- run.py
+++ run.py
@@ -2,15 +2,19 @@ from vision import Vision
 from control import Controller
 from raid import Raid
 from log import log
+import argparse
+
 
 vision = Vision()
 controller = Controller()
 raid = Raid(vision, controller)
 
-
 def run():
-    num_raid = int(input('How many raids to run? '))
-    for i in range(num_raid):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--x', type=int, default=1, help='Number of raids')
+    args = parser.parse_args()    
+
+    for i in range(args.x):
         log(f'Start Raid {i+1}')
         raid.run()
         log(f'Raid {i+1} is done')
