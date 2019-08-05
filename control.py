a23827883fa2a35f18865f480b5e09c78852b5ba
hieutrpham <37964079+hieutrpham@users.noreply.github.com>
2019-08-05 19:32:10 -0400

    remove useless screenshots and add some coordinate constants

--- control.py
+++ control.py
@@ -3,6 +3,14 @@ import time
 from log import log
 
 
+# Coordinate constants that will be used in controller class 
+RAID = (496, 701)
+SUMMON = (1124, 761)
+HEROIC = (1183, 592)
+RAID_ACCEPT = (1097, 868)
+AUTO_BUTTON = (1432, 654)
+
+
 class Controller:
 
     def leftClick(self, x, y):
@@ -21,23 +29,23 @@ class Controller:
         self.leftClick(448 + 756, 329 + 136)
 
     def click_raid(self):
-        self.leftClick(492, 735)
+        self.leftClick(RAID[0], RAID[1])
         time.sleep(1.5)
 
     def click_summon(self):
-        self.leftClick(448 + 679, 329 + 471)
+        self.leftClick(SUMMON[0], SUMMON[1])
         time.sleep(1.5)
 
     def click_heroic(self):
-        self.leftClick(448 + 738, 329 + 302)
+        self.leftClick(HEROIC[0], HEROIC[1])
         time.sleep(1.5)
 
     def click_accept(self):
-        self.leftClick(448 + 649, 329 + 580)
+        self.leftClick(RAID_ACCEPT[0], RAID_ACCEPT[1])
 
     def click_auto(self):
         log('Auto on.')
-        self.leftClick(1432, 654)
+        self.leftClick(AUTO_BUTTON[0], AUTO_BUTTON[1])
 
     def click_potion(self):
         self.leftClick(448 + 54, 329 + 517)
