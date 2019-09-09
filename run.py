41187247a37fdb390dbc170098525bf56db98c21
hieutrpham <37964079+hieutrpham@users.noreply.github.com>
2019-09-09 07:48:56 -0400

    add some summary stats to run

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from control import Controller
 from raid import Raid
 from log import log
 import argparse
+import time
 
 
 vision = Vision()
@@ -17,16 +18,25 @@ def run():
     log(f'Total raids to run: {args.num_raid}')
 
     defeat_num = 0
+    duration_list = []
 
     for i in range(args.num_raid):
+        start = time.time()
+
         log(f'Start Raid {i+1}')
         x = raid.run()
         if x == 1:
             defeat_num += 1
-        log(f'Raid {i+1} is done')
+
+        end = time.time()
+
+        duration = end - start
+        duration_list.append(duration)
+        log(f"Raid {i+1} is done in {time.strftime('%M:%S', time.gmtime(duration))}")
 
     print(f'Success rate: {(args.num_raid - defeat_num)/args.num_raid * 100}%')
     print(f'Total defeat: {defeat_num} out of {args.num_raid} runs')
+    print(f"Average run time per raid: {time.strftime('%M:%S', time.gmtime(sum(duration_list)/len(duration_list)))}")
 
 if __name__ == '__main__':
     run()
